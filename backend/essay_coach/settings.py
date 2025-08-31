d4290739c837597b439463db307d75d20cb9aaf8
Eric <xyan0147@student.monash.edu>
2025-09-01 02:18:16 +0800

    feat(backend): implement token-based authentication and configure DRF
    - Add rest_framework.authtoken to INSTALLED_APPS for token-based authentication
    - Configure CORS_ALLOWED_ORIGINS and CSRF_TRUSTED_ORIGINS for local development- Set up global DRF settings including pagination, datetime format, renderers, parsers, permissions, and authentication classes

--- backend/essay_coach/settings.py
+++ backend/essay_coach/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
+    "rest_framework.authtoken", # For token-based authentication
     "django_filters",
     # Custom Apps
     "core",
@@ -67,6 +68,30 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = ["http://localhost:5100"]
 CSRF_TRUSTED_ORIGINS = ['http://localhost:5100']
+
+# Django REST Framework settings (DRF global)
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 50,
+    "DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S",
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+    "rest_framework.parsers.JSONParser",
+    "rest_framework.parsers.FormParser",
+    "rest_framework.parsers.MultiPartParser",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+}
 
 ROOT_URLCONF = "essay_coach.urls"
 
