d53122b3669820c0746875e8cab9342f6bfbb888
2333xxxx <ctc20120114@163.com>
2025-12-15 23:52:47 +0800

    fix bug

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -4,24 +4,32 @@ export default async function middleware(req: NextRequest) {
   const { nextUrl, cookies } = req;
   const pathname = nextUrl.pathname;
 
-  // Bypass our checks for auth endpoints and static assets
-  if (pathname.includes('/auth/login/')) return NextResponse.next();
-  if (pathname.startsWith('/_next')) return NextResponse.next();
-
-  // const isProtected = pathname.startsWith('/dashboard');
-  // if (!isProtected) return NextResponse.next();
-
-  // all sites except login require authentication
   const access = cookies.get('access_token')?.value;
   if (access) {
+    // Redirect to dashboard if user is already authenticated and tries to access sign-in page
+    if (pathname.startsWith('/auth/sign-in')) {
+      const url = req.nextUrl.clone();
+      url.pathname = '/dashboard/overview';
+      return NextResponse.redirect(url);
+    }
+
+    // Attach Authorization header for authenticated requests
     const headers = new Headers(req.headers);
     headers.set('Authorization', `Bearer ${access}`);
-
     return NextResponse.next({
       request: {
         headers
       }
     });
+  }
+
+  // Bypass for public routes
+  if (
+    pathname.startsWith('/auth/sign-in') || // frontend login page
+    pathname.startsWith('/_next') ||
+    pathname.includes('/api/auth/login') // backend login endpoint
+  ) {
+    return NextResponse.next();
   }
 
   // Redirect to login if no access token is present
